83eb5ff8c3a95738c0d39b1a56222490901c5587
Artjoms Dobiss <archie.dobiss@gmail.com>
2022-01-31 20:47:31 +0000

    Update file

--- nltk_functions.py
+++ nltk_functions.py
@@ -1,4 +1,5 @@
 import nltk
+import re
 
 def lexical_diversity(text):
     return len(text) / len(set(text))
@@ -33,6 +34,17 @@ def generate_model(cfd, word, num=15):
 def stress(pron):
     return [char for phone in pron for char in phone if char.isdigit()] # whatever is in front of 1st FOR statement
                                                                         # is dded to the list
+
+def compress(word):
+    regexp = r'^[AEIOUaeiou]+|[AEIOUaeiou]+$|[^AEIOUaeiou]'
+    pieces = re.findall(regexp, word)
+    return ''.join(pieces)
+
+def stem(word):
+    regexp = r'(^.*?)(ly|ing|ed|ious|ies|ive|es|s|ment)?$'
+    stem, suffix = re.findall(regexp, word)[0]
+    return stem
+
 
 wordlist = nltk.corpus.words.words()
 stopwords = nltk.corpus.stopwords.words('russian')
